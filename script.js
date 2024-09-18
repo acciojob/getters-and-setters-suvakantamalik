b833364ca2d5f95b5f3eb8ff51ce6c7b2a955493
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-09-18 15:13:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,13 +17,13 @@ class Person {
 
 class Student extends Person {
 	study(){
-		console.log(`${this.name} is studying`);
+		console.log(`${this._name} is studying`);
 	}
 }
 
 class Teacher extends Person {
 	teach(){
-		console.log(`${this.name} is teaching`)
+		console.log(`${this._name} is teaching`)
 	}
 }
 
